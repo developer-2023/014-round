03f3da9ad281f3c8a80b24f626370d4ca97ab9e5
developer-2023 <132809345+developer-2023@users.noreply.github.com>
2023-11-05 21:12:06 +0100

    Initial commit

---
+++ 014-round.py
@@ -0,0 +1,8 @@
+# docs.python.org/3/library/functions.html#round
+
+# Enter two strings and convert them into floats
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+# add them and show result
+print(round(x + y,2))
